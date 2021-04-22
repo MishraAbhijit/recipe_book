b81641f0fa842ddc41cf7b54677b4f58358537ad
Abhijit Mishra <m.abhijitmishra97@gmail.com>
2021-04-22 22:54:44 +0530

    init

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h2 className='section-title'>about us</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi dolore provident reprehenderit possimus suscipit delectus, nemo quod sequi maxime dolor rem debitis commodi voluptatum cumque vero labore harum dignissimos nulla?</p>
+    </section>
+  )
+}
+
+export default About
